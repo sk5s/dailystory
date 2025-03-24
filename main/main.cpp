51b6e91e2fb71d9dcb6af2538445e432b18ab62a
sk5s <samko5sam@tutanota.com>
2025-03-24 10:11:43 +0800

    ✨ CEF example

--- main/main.cpp
+++ main/main.cpp
@@ -1,61 +1,221 @@
+/*******************************************************************************
+ *   Copyright (c) 2013 Álan Crístoffer
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a
+ *   copy of this software and associated documentation files (the "Software"),
+ *   to deal in the Software without restriction, including without limitation
+ *   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *   and/or sell copies of the Software, and to permit persons to whom the
+ *   Software is furnished to do so, subject to the following conditions:
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *   DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+
+#include <string>
+#include <algorithm>
 #include <windows.h>
-#include "include/cef_command_line.h"
-#include "include/cef_sandbox_win.h"
-#include "simple_app.hpp"
 
-#if defined(CEF_USE_SANDBOX)
-#pragma comment(lib, "cef_sandbox.lib")
-#endif
+#include "include/cef_app.h"
+#include "include/cef_browser.h"
+#include "ClientApp.hpp"
+#include "ClientHandler.hpp"
 
-// Correct signature for wWinMain.
-int WINAPI wWinMain(HINSTANCE hInstance,
-                    HINSTANCE hPrevInstance,
-                    PWSTR pCmdLine, // Changed from LPTSTR to PWSTR
-                    int nCmdShow) {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(pCmdLine);
+ClientHandler *g_handler = 0;
 
-    int exit_code;
+std::string GetApplicationDir()
+{
+	HMODULE hModule = GetModuleHandleW(NULL);
+	WCHAR wpath[MAX_PATH];
 
-#if defined(ARCH_CPU_32_BITS)
-    exit_code = CefRunWinMainWithPreferredStackSize(wWinMain, hInstance, pCmdLine, nCmdShow);
-    if (exit_code >= 0) {
-        return exit_code;
-    }
-#endif
+	GetModuleFileNameW(hModule, wpath, MAX_PATH);
+	std::wstring wide(wpath);
 
-    void* sandbox_info = nullptr;
+	std::string path = CefString(wide);
+	path = path.substr(0, path.find_last_of("\\/"));
+	return path;
+}
 
-#if defined(CEF_USE_SANDBOX)
-    CefScopedSandboxInfo scoped_sandbox;
-    sandbox_info = scoped_sandbox.sandbox_info();
-#endif
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg) {
+	case WM_DESTROY:
+		CefQuitMessageLoop();
+		PostQuitMessage(0);
+		return 0;
 
-    CefMainArgs main_args(hInstance);
+	case WM_SIZE:
+		if (g_handler) {
+			// Resize the browser window and address bar to match
+			//  the new frame
+			// window size
+			RECT rect;
+			GetClientRect(hwnd, &rect);
+			HDWP hdwp = BeginDeferWindowPos(1);
+			hdwp = DeferWindowPos(hdwp, g_handler->GetBrowserHwnd(),
+			                      NULL, rect.left, rect.top,
+			                      rect.right - rect.left,
+			                      rect.bottom - rect.top,
+			                      SWP_NOZORDER);
+			EndDeferWindowPos(hdwp);
+		}
 
-    exit_code = CefExecuteProcess(main_args, nullptr, sandbox_info);
-    if (exit_code >= 0) {
-        return exit_code;
-    }
+		break;
 
-    CefRefPtr<CefCommandLine> command_line = CefCommandLine::CreateCommandLine();
-    command_line->InitFromString(::GetCommandLineW());
+	case WM_ERASEBKGND:
+		if (g_handler) {
+			// Dont erase the background if the browser window has
+			//  been loaded
+			// (this avoids flashing)
+			return 0;
+		}
 
-    CefSettings settings;
+		break;
 
-#if !defined(CEF_USE_SANDBOX)
-    settings.no_sandbox = true;
-#endif
+	case WM_PAINT:
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hwnd, &ps);
+		EndPaint(hwnd, &ps);
+		return 0;
+	}
 
-    CefRefPtr<SimpleApp> app = new SimpleApp;
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
 
-    if (!CefInitialize(main_args, settings, app.get(), sandbox_info)) {
-        return CefGetExitCode();
-    }
+HWND RegisterWindow(HINSTANCE hInstance, int nCmdShow)
+{
+	WNDCLASS wc = {};
 
-    CefRunMessageLoop();
+	wc.lpfnWndProc   = WindowProc;
+	wc.hInstance     = hInstance;
+	wc.lpszClassName = "TestsDatabaseWindow";
+	RegisterClass(&wc);
+	HWND hwnd = CreateWindowEx(0,                                 // Optional
+	                                                              //  window
+	                                                              //  styles.
+	                           "TestsDatabaseWindow", // Window class
+	                           "Daily Story", // Window text
+	                           WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN, // Window
+	                                                                  //  style
+	                           // Size and position
+	                           CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+	                           CW_USEDEFAULT,
+	                           NULL, // Parent window
+	                           NULL, // Menu
+	                           hInstance, // Instance handle
+	                           NULL // Additional application data
+	                           );
 
-    CefShutdown();
+	if (hwnd == NULL) {
+		return 0;
+	}
 
-    return 0;
+	ShowWindow(hwnd, nCmdShow);
+	return hwnd;
+}
+
+LRESULT CALLBACK MessageWndProc(HWND hWnd, UINT message, WPARAM wParam,
+                                LPARAM lParam)
+{
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+HWND CreateMessageWindow(HINSTANCE hInstance)
+{
+	WNDCLASSEX wc = {
+		0
+	};
+
+	wc.cbSize        = sizeof(wc);
+	wc.lpfnWndProc   = MessageWndProc;
+	wc.hInstance     = hInstance;
+	wc.lpszClassName = "ClientMessageWindow";
+	RegisterClassEx(&wc);
+	return CreateWindow("ClientMessageWindow", 0, 0, 0, 0, 0, 0,
+	                    HWND_MESSAGE, 0, hInstance, 0);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+{
+	CefMainArgs main_args(hInstance);
+
+	CefRefPtr<ClientApp> app(new ClientApp);
+
+	// Execute the secondary process, if any.
+	int exit_code = CefExecuteProcess(main_args, app.get(), NULL);
+
+	if (exit_code >= 0) {
+		exit(exit_code);
+	}
+
+	// Register the window class.
+	HWND hwnd = RegisterWindow(hInstance, nCmdShow);
+	if (hwnd == 0) {
+		return 0;
+	}
+
+	RECT rect;
+	GetClientRect(hwnd, &rect);
+
+	// 轉換為 CefRect
+	CefRect cef_rect(rect.left, rect.top,
+										rect.right - rect.left,
+										rect.bottom - rect.top);
+
+	CefSettings settings;
+	settings.no_sandbox = true;
+	CefInitialize(main_args, settings, app.get(), NULL);
+	CefWindowInfo info;
+	CefBrowserSettings b_settings;
+	CefRefPtr<CefClient> client(new ClientHandler);
+	g_handler = (ClientHandler*) client.get();
+	// std::string path         = "file://" + GetApplicationDir() +
+	//                            "/dist/index.html";
+	std::string path = "http://localhost:5173";
+	CefRefPtr<CefCommandLine> command_line =
+		CefCommandLine::GetGlobalCommandLine();
+
+	if (command_line->HasSwitch("url")) {
+		path = command_line->GetSwitchValue("url");
+	}
+
+	// command_line->AppendSwitch("allow-running-insecure-content");
+	// command_line->AppendSwitch("enable-media-stream");
+	// command_line->AppendSwitch("disable-web-security");
+	// command_line->AppendSwitch("enable-local-file-accesses");
+
+	info.SetAsChild(hwnd, cef_rect);
+	CefBrowserHost::CreateBrowser(info,
+	                              client.get(), path, b_settings, nullptr,
+	                              nullptr);
+	int result = 0;
+
+	if (!settings.multi_threaded_message_loop) {
+		// Run the CEF message loop. This function will block until the
+		//  application
+		// recieves a WM_QUIT message.
+		CefRunMessageLoop();
+	} else {
+		// Create a hidden window for message processing.
+		HWND hMessageWnd = CreateMessageWindow(hInstance);
+		MSG msg;
+
+		// Run the application message loop.
+		while (GetMessage(&msg, NULL, 0, 0)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		DestroyWindow(hMessageWnd);
+		hMessageWnd = NULL;
+		result      = static_cast<int>(msg.wParam);
+	}
+
+	CefShutdown();
+	return result;
 }
