00e7e8bbe8ebc835a41280beec4cd7b41894b888
sk5s <samko5sam@tutanota.com>
2025-04-10 01:19:54 +0800

    ✨ Big Calendar, KiboEditor test

--- web/src/components/features/calendar/FullPageCalendarView.tsx
+++ web/src/components/features/calendar/FullPageCalendarView.tsx
@@ -1,5 +1,44 @@
+import {
+  CalendarBody,
+  CalendarDate,
+  CalendarDatePagination,
+  CalendarDatePicker,
+  CalendarHeader,
+  CalendarItem,
+  CalendarMonthPicker,
+  CalendarProvider,
+  CalendarYearPicker,
+} from '@/components/ui/kibo-ui/calendar';
+import { exampleFeatures } from '@/lib/content';
+
 export const FullPageCalendarView = () => {
+  const earliestYear =
+    exampleFeatures
+      .map((feature) => feature.startAt.getFullYear())
+      .sort()
+      .at(0) ?? new Date().getFullYear();
+
+  const latestYear =
+    exampleFeatures
+      .map((feature) => feature.endAt.getFullYear())
+      .sort()
+      .at(-1) ?? new Date().getFullYear();
+
   return (
-    <div>FullPageCalendarView</div>
+    <div className='h-full'>
+      <CalendarProvider locale="zh-TW">
+        <CalendarDate>
+          <CalendarDatePicker>
+            <CalendarMonthPicker />
+            <CalendarYearPicker start={earliestYear} end={latestYear} />
+          </CalendarDatePicker>
+          <CalendarDatePagination />
+        </CalendarDate>
+        <CalendarHeader />
+        <CalendarBody features={exampleFeatures}>
+          {({ feature }) => <CalendarItem key={feature.id} feature={feature} />}
+        </CalendarBody>
+      </CalendarProvider>
+    </div>
   )
 }
